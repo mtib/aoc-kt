30dafa07815ee817b45e74cd601b5f00b6262c9d
Markus Becker <mabe@monta.com>
2024-12-02 19:13:58 +0100

    perf: parallel day 2

--- src/main/kotlin/dev/mtib/aoc/aoc24/days/Day2.kt
+++ src/main/kotlin/dev/mtib/aoc/aoc24/days/Day2.kt
@@ -1,58 +1,54 @@
 package dev.mtib.aoc.aoc24.days
 
 import dev.mtib.aoc.day.AocDay
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
-import kotlin.math.abs
 
 object Day2 : AocDay(2024, 2) {
     private fun List<Int>.isTotallySafe(): Boolean {
-        for (v in indices) {
-            val last2 = getOrNull(v - 2)
-            val last1 = getOrNull(v - 1)
-            val current = this[v]
-
-            if (last2 != null && last1 != null) {
-                if (last2 < last1 && last1 > current) {
-                    return false
-                }
-                if (last2 > last1 && last1 < current) {
-                    return false
-                }
+        val up = (this[1] - this[0]) > 0
+        var last1: Int = this[0]
+        var current: Int
+        for (v in 1 until this.size) {
+            current = this[v]
+            val diff = current - last1
+            if ((diff !in 1..3 && diff !in -3..-1) || (up != (diff > 0))) {
+                return false
             }
-
-            if (last1 != null) {
-                if (abs(last1 - current).let {
-                        it == 0 || it > 3
-                    }) {
-                    return false
-                }
-            }
+            last1 = current
         }
         return true
     }
 
-    override suspend fun part1(): String = coroutineScope {
-        inputLinesList.filter {
-            it.split(" ").map { it.toInt() }.isTotallySafe()
-        }.size.toString()
+    override suspend fun part1(): Int = coroutineScope {
+        inputLinesList.chunked(100).map { lines ->
+            async {
+                lines.count { line -> line.split(" ").map { it.toInt() }.isTotallySafe() }
+            }
+        }.awaitAll().sum()
     }
 
-    override suspend fun part2(): String = coroutineScope {
-        inputLinesList.filter {
-            val report = it.split(" ").map { it.toInt() }
+    override suspend fun part2(): Int = coroutineScope {
+        inputLinesList.chunked(100).map { lines ->
+            async {
+                lines.count {
+                    val report = it.split(" ").map { it.toInt() }
 
-            if (report.isTotallySafe()) {
-                return@filter true
-            }
+                    if (report.isTotallySafe()) {
+                        return@count true
+                    }
 
-            for (removed in report.indices) {
-                val removedReport = report.toMutableList().also { it.removeAt(removed) }
-                if (removedReport.isTotallySafe()) {
-                    return@filter true
+                    for (removed in report.indices) {
+                        val removedReport = report.toMutableList().also { it.removeAt(removed) }
+                        if (removedReport.isTotallySafe()) {
+                            return@count true
+                        }
+                    }
+
+                    false
                 }
             }
-
-            false
-        }.size.toString()
+        }.awaitAll().sum()
     }
 }
---
+++ src/main/kotlin/dev/mtib/aoc/aoc24/days/Day3.kt
@@ -0,0 +1,3 @@
+package dev.mtib.aoc.aoc24.days
+
+// object Day3 : AocDay(2024, 3)
