7bf99f3850c5541b948f47751535701081f07f65
Markus Becker <mtib.becker@gmail.com>
2024-12-04 12:01:12 +0100

    fix: minimum window size

--- src/main/kotlin/dev/mtib/aoc/util/ListExtensions.kt
+++ src/main/kotlin/dev/mtib/aoc/util/ListExtensions.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 
 suspend fun <T, R> Iterable<T>.chunkedParMap(chunkSize: Int, block: suspend (List<T>) -> R): List<R> = coroutineScope {
-    chunked(chunkSize).map { chunk ->
+    chunked(chunkSize.coerceAtLeast(1)).map { chunk ->
         async {
             block(chunk)
         }
@@ -16,7 +16,7 @@ suspend fun <T, R> Iterable<T>.chunkedParMap(chunkSize: Int, block: suspend (Lis
  * This is stateless and terminal.
  */
 suspend fun <T, R> Sequence<T>.chunkedParMap(chunkSize: Int, block: suspend (List<T>) -> R): List<R> = coroutineScope {
-    chunked(chunkSize).map { chunk ->
+    chunked(chunkSize.coerceAtLeast(1)).map { chunk ->
         async {
             block(chunk)
         }
