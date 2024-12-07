b07fc5885e80582af46e005f0c8c9951fcf04391
Markus Becker <mtib.becker@gmail.com>
2024-12-07 11:24:06 +0100

    feat: day 7, very parallel, very bad

--- src/main/kotlin/dev/mtib/aoc/aoc24/days/Day7.kt
+++ src/main/kotlin/dev/mtib/aoc/aoc24/days/Day7.kt
@@ -1,13 +1,104 @@
 package dev.mtib.aoc.aoc24.days
 
 import dev.mtib.aoc.day.AocDay
+import dev.mtib.aoc.util.chunkedParMap
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.selects.select
+import java.math.BigInteger
 
 object Day7: AocDay(2024, 7) {
-    override suspend fun part1(): Any {
-        return super.part1()
+
+    private suspend fun bruteForce(goal: BigInteger, parts: List<BigInteger>, current: BigInteger? = null, withPart2: Boolean = false, scope: CoroutineScope, channel: SendChannel<Unit>? = null): Boolean {
+        if (current == null) {
+            val channel = Channel<Unit>(Channel.BUFFERED)
+            val work = scope.launch {
+                bruteForce(
+                    goal = goal,
+                    parts = parts.subList(1, parts.size),
+                    current = parts[0],
+                    withPart2 = withPart2,
+                    scope = scope,
+                    channel = channel
+                )
+            }
+            return select<Boolean> {
+                channel.onReceive {
+                    work.cancel()
+                    true
+                }
+                work.onJoin {
+                    false
+                }
+            }
+        }
+
+        if (current > goal) {
+            return false
+        }
+
+        if (parts.isEmpty()) {
+            if (current == goal) {
+                channel?.send(Unit)
+            }
+            return current == goal
+        }
+
+        val jobs = buildSet<Deferred<Boolean>> {
+             add(scope.async { bruteForce(goal, parts.subList(1, parts.size), current + parts[0], withPart2 = withPart2, scope = scope, channel = channel)})
+             add(scope.async { bruteForce(goal, parts.subList(1, parts.size), current * parts[0], withPart2 = withPart2, scope = scope, channel = channel)})
+            if (withPart2){
+                add(scope.async { bruteForce(goal, parts.subList(1, parts.size), (current.toString() + parts[0].toString()).toBigInteger(), withPart2 = true, scope = scope, channel = channel)})
+            }
+        }.toMutableSet()
+
+        while (jobs.isNotEmpty()) {
+            select {
+                jobs.forEach { job ->
+                    job.onAwait { result ->
+                        jobs.remove(job)
+                        if (result) {
+                            channel?.send(Unit)
+                        }
+                        result
+                    }
+                }
+            }.let {
+                if (it) {
+                    jobs.forEach { it.cancel() }
+                    return true
+                }
+            }
+        }
+
+        return false
     }
 
-    override suspend fun part2(): Any {
-        return super.part2()
+    private val parseRegex = Regex("""^(\d+):(?: (\d+))*""")
+    override suspend fun part1(): Any = coroutineScope {
+        inputLinesList.chunkedParMap(inputLinesList.size / cpu) {lines ->
+            lines.map {
+                val colon = it.indexOf(':')
+                val goal = it.substring(0, colon).toBigInteger()
+                val parts = it.substring(colon + 2).split(" ").map { it.toBigInteger() }
+                goal to parts
+            }.filter { bruteForce(it.first, it.second, scope = this@coroutineScope) }
+                .map { it.first }
+                .fold (BigInteger.ZERO) { acc, i -> acc + i }
+        }.fold(BigInteger.ZERO) { acc, i -> acc + i }
+    }
+
+    override suspend fun part2() = coroutineScope {
+        inputLinesList.chunkedParMap(inputLinesList.size / cpu) {lines ->
+            lines.map {
+                val colon = it.indexOf(':')
+                val goal = it.substring(0, colon).toBigInteger()
+                val parts = it.substring(colon + 2).split(" ").map { it.toBigInteger() }
+                goal to parts
+            }.filter { bruteForce(it.first, it.second, withPart2 = true, scope = this@coroutineScope) }
+                .map { it.first }
+                .fold (BigInteger.ZERO) { acc, i -> acc + i }
+        }.fold(BigInteger.ZERO) { acc, i -> acc + i }
     }
 }
---
+++ src/test/kotlin/dev/mtib/aoc/aoc24/days/Day7Test.kt
@@ -0,0 +1,35 @@
+package dev.mtib.aoc.aoc24.days
+
+import io.kotest.core.spec.style.FunSpec
+
+class Day7Test : FunSpec({
+    val snippet0 = """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+    """.trimIndent()
+    context("part1") {
+        test("doesn't throw") {
+            try {
+                Day7.part1()
+            } catch (e: NotImplementedError) {
+                // Ignore allowed exception
+            }
+        }
+    }
+    context("part2") {
+        test("doesn't throw") {
+            try {
+                Day7.part2()
+            } catch (e: NotImplementedError) {
+                // Ignore allowed exception
+            }
+        }
+    }
+})
