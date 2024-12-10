168371abee0199d7a18074b49515643f1fc9177a
Markus Becker <mtib.becker@gmail.com>
2024-12-10 13:48:26 +0100

    perf: replace BigInteger with Long

--- src/main/kotlin/dev/mtib/aoc/aoc24/days/Day7.kt
+++ src/main/kotlin/dev/mtib/aoc/aoc24/days/Day7.kt
@@ -2,16 +2,11 @@ package dev.mtib.aoc.aoc24.days
 
 import dev.mtib.aoc.day.AocDay
 import dev.mtib.aoc.util.chunkedParMap
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.SendChannel
-import kotlinx.coroutines.selects.select
-import java.math.BigDecimal
 import java.math.BigInteger
 import kotlin.math.log10
 
 object Day7: AocDay(2024, 7) {
-    private suspend fun bruteForce(goal: BigInteger, parts: List<BigInteger>, current: BigInteger? = null, withPart2: Boolean = false): Boolean {
+    private suspend fun bruteForce(goal: Long, parts: List<Long>, current: Long? = null, withPart2: Boolean = false): Boolean {
         if (current == null) {
             return bruteForce(
                 goal = goal,
@@ -30,7 +25,7 @@ object Day7: AocDay(2024, 7) {
         }
 
         return bruteForce(goal, parts.subList(1, parts.size), current * parts[0], withPart2 = withPart2) ||
-                (withPart2 && bruteForce(goal, parts.subList(1, parts.size), current * BigInteger.TEN.pow(log10(parts[0].toDouble()).toInt() + 1) + parts[0], withPart2 = true)) ||
+                (withPart2 && bruteForce(goal, parts.subList(1, parts.size), current * BigInteger.TEN.pow(log10(parts[0].toDouble()).toInt() + 1).toLong() + parts[0], withPart2 = true)) ||
                 bruteForce(goal, parts.subList(1, parts.size), current + parts[0], withPart2 = withPart2)
     }
 
@@ -38,13 +33,13 @@ object Day7: AocDay(2024, 7) {
         val matching = inputLinesList.chunkedParMap(inputLinesList.size / cpu) {lines ->
             lines.map {
                 val colon = it.indexOf(':')
-                val goal = it.substring(0, colon).toBigInteger()
-                val parts = it.substring(colon + 2).split(" ").map { it.toBigInteger() }
+                val goal = it.substring(0, colon).toLong()
+                val parts = it.substring(colon + 2).split(" ").map { it.toLong() }
                 goal to parts
             }.filter { bruteForce(it.first, it.second) }
                 .map { it.first }
-                .fold (BigInteger.ZERO) { acc, i -> acc + i }
-        }.fold(BigInteger.ZERO) { acc, i -> acc + i }
+                .fold (0L) { acc, i -> acc + i }
+        }.fold(0L) { acc, i -> acc + i }
         return matching
     }
 
@@ -52,13 +47,13 @@ object Day7: AocDay(2024, 7) {
         val matching = inputLinesList.chunkedParMap(inputLinesList.size / cpu) {lines ->
             lines.map {
                 val colon = it.indexOf(':')
-                val goal = it.substring(0, colon).toBigInteger()
-                val parts = it.substring(colon + 2).split(" ").map { it.toBigInteger() }
+                val goal = it.substring(0, colon).toLong()
+                val parts = it.substring(colon + 2).split(" ").map { it.toLong() }
                 goal to parts
             }.filter { bruteForce(it.first, it.second, withPart2 = true) }
                 .map { it.first }
-                .fold (BigInteger.ZERO) { acc, i -> acc + i }
-        }.fold(BigInteger.ZERO) { acc, i -> acc + i }
+                .fold (0L) { acc, i -> acc + i }
+        }.fold(0L) { acc, i -> acc + i }
         return matching
     }
 }
